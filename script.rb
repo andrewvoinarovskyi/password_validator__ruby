fb6ec6cae4b9b2ad33d75e04e15400aeb30aac4e
andrii <avoinarovskiy@gmail.com>
2023-07-21 13:00:05 +0300

    parse text from file into pairs of password-condition

--- script.rb
+++ script.rb
@@ -5,6 +5,8 @@ textline_passwords = file.read()
 
 # parse line into condition-password pairs
 
+passwords_with_conditions = textline_passwords.split(/\n/)
+condition_password_pair = passwords_with_conditions.map { |line| line.split(/\:\s/) }
 
 # check all rules
 
