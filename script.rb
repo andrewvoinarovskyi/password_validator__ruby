7180a8b5e2131011eedf60f3d60169a70e4e6a48
andrii <avoinarovskiy@gmail.com>
2023-07-21 14:53:09 +0300

    add check for compliance with the condition

--- script.rb
+++ script.rb
@@ -9,7 +9,15 @@ passwords_with_conditions = textline_passwords.split(/\n/)
 condition_password_pair = passwords_with_conditions.map { |line| line.split(/\:\s/) }
 
 # check all rules
+result = 0
 
+condition_password_pair.each { |pair|
+  password = pair[1]
+  sign, cond = pair[0].split(' ')
+  min, max = cond.split('-')
+  if password.count(sign).between?(min.to_i, max.to_i)
+    result += 1
+  end
+}
 
-
-# puts result;
+puts result
